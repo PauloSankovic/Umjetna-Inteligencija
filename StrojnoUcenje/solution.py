b9e04a5b2cefeda99049078302596ea22210ea4b
Paulo Sanković <paulo.sankovic@fer.hr>
2020-05-30 17:02:16 +0200

    Implemented 'Evaluating model performance'

--- StrojnoUcenje/solution.py
+++ StrojnoUcenje/solution.py
@@ -33,12 +33,12 @@ def parse_test_set(file):
         else:
             parts = line.split(',')
             test_dataset.append(tuple(parts[:len(header) - 1]))
-            results.append(parts[len(header) - 1:])
+            results.append(parts[-1])
 
     return test_dataset, results
 
 
-def parse_config_file(file):
+def parse_config_file(file) -> dict:
     lines = open(file, 'r', encoding='utf-8').read().splitlines()
 
     config = {'max_depth': float('Inf'), 'num_trees': int(1), 'feature_ratio': float(1), 'example_ratio': float(1)}
@@ -52,6 +52,34 @@ def parse_config_file(file):
             config[parts[0]] = parts[1]
 
     return config
+
+
+def get_accuracy(predictions: list, results: list) -> float:
+    count = 0
+    for i in range(len(results)):
+        if predictions[i] == results[i]:
+            count += 1
+
+    return count / len(results)
+
+
+def confusion_matrix(predictions: list, results: list, classes: list):
+    n = len(classes)
+    matrix = [[0 for x in range(n)] for y in range(n)]
+    for i in range(n):
+        indexes = []
+        for index in range(len(results)):
+            if results[index] == classes[i]:
+                indexes.append(index)
+
+        for j in range(len(classes)):
+            count = 0
+            for index in indexes:
+                if predictions[index] == classes[j]:
+                    count += 1
+            matrix[i][j] = count
+
+    return matrix
 
 
 def main(argv):
@@ -68,6 +96,15 @@ def main(argv):
     print(predictions[0], end="")
     for i in range(1, len(predictions)):
         print(f" {predictions[i]}", end="")
+    print()
+
+    print("{:.5f}".format(get_accuracy(predictions, results)))
+
+    matrix = confusion_matrix(predictions, results, list(set(train_dataset.values())))
+    for x in matrix:
+        for y in x:
+            print(y, end=" ")
+        print()
 
 
 if __name__ == "__main__":
